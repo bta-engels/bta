8d980b372a87b7e0b71c764554e5d50fafd35a43
berndengels <99887389+bta-engels@users.noreply.github.com>
2023-03-05 19:16:25 +0100

    initial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        URL::forceScheme(env('FORCE_SCHEME', 'http'));
+        Schema::defaultStringLength(191);
+        Paginator::useTailwind();
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import fs from 'fs';
+const host = 'bta.test';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`./nginx/certs/${host}.key`),
+            cert: fs.readFileSync(`./nginx/certs/${host}.pem`),
+        },
+    },
 });
